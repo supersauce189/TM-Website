793a8c30a99b4c3779eebc41a943ac87fea8d7bd
supersauce189 <williamlelexing@gmail.com>
2025-04-21 19:21:34 -0700

    Worked on teams.html, clinic.html

---
+++ Team Pages/swiper.js
@@ -0,0 +1,19 @@
+window.addEventListener('load', () => {
+    const swiper = new Swiper('.swiper', {
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: true,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        observer: true,
+        observeParents: true,
+    });
+});
