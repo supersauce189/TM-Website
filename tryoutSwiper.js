d9777f4a4dcb4365b94c8fd7dede23ab64fb3839
supersauce189 <williamlelexing@gmail.com>
2025-05-10 09:48:40 -0700

    Small fixes

---
+++ tryoutSwiper.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', () => {
+    const swiper = new Swiper('.swiper', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        observer: true,
+        observeParents: true,
+    });
+});
